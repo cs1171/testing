131a7356dbe1cffe23b5d45c91f20943a8788a50
cs1171 <cdsoto2@miners.utep.edu>
2016-09-22 01:40:27 -0600

    worked on header and file scanning

--- btree/btree.c
+++ btree/btree.c
@@ -1,9 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 #include "btree.h"
+#define pmalloc (struct Node *) malloc(sizeof(struct Node));
 
-int main()
+void populate_tree(struct Node ** tree, char *filename);
+
+int main(void)
 {
+  struct Node *tree = NULL;
+  populate_tree(&tree, "names.txt");
+  //printf("First name   Last name   Age\n");
+  //printf("----------------------------\n");
+  //printTree(tree);
+  //freeTree(tree);
+}
+
+void populate_tree(struct Node ** tree, char * filename)
+{
+  FILE *file = fopen(filename, "rt");
+  struct Node * leaf = NULL;
+  struct Node * current = NULL;
   
-  return 0;
+  // catch any file errors
+  if(!file)
+    {
+      printf("Error opening file\n%s\n", strerror(errno));
+      exit(1);
+    }
+
+  // reading names
+  current = pmalloc;
+  while(fscanf(file, "%20s", current->first_name) > 0)
+    {
+      fscanf(file, "%20s", current->last_name);
+
+      // testing fscanf
+      // printf("%s %s\n", current->first_name, current->last_name);
+
+      
+    }
 }
--- btree/btree.h
+++ btree/btree.h
@@ -2,18 +2,11 @@
 #define btree
 #define NAME_LENGTH 20
 
-typedef struct
-{
-  Node *left,*right;
-
+struct Node {
   char first_name[NAME_LENGTH + 1];
   char last_name[NAME_LENGTH + 1];
-} Node;
-
-Node *btreeAlloc();
-void btreeFree(Node *btree);
-void add(Node *n);
-void remove(Node *n);
-void printAll(Node *n);
+  
+  struct Node* right,*left;
+};
 
 #endif
